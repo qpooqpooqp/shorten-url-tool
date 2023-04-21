b9cea52099b954852d3e24b96050daf5e900495a
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-04-21 11:34:18 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('喔幹，資料庫連線失敗...')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('喔耶~資料庫連線成功!')
+})
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
